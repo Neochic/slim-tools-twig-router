24e8ab023f2927ed81de837b79bc28a632e7ad5a
Christoph Stickel <christoph@neochic.de>
2015-05-06 12:37:04 +0200

    * fixed namespace of TwigView * fixed typo

--- src/Service/TwigView.php
+++ src/Service/TwigView.php
@@ -1,9 +1,9 @@
 <?php
-namespace Neochic\SlimTools\Factory;
+namespace Neochic\SlimTools\Service;
 
 class TwigView extends \Slim\Views\Twig {
     public function __construct(array $twigConfig, array $coreConfig) {
-        $this->parserOptions['debug'] = $coreConfig['mod'] === 'dev'; //activate debug mode only for development mode
+        $this->parserOptions['debug'] = $coreConfig['mode'] === 'dev'; //activate debug mode only for development mode
         $this->parserOptions['cache'] = $twigConfig['cacheDir'];
         $this->parserExtensions = array_slice(func_get_args(), 2);
         $this->twigTemplateDirs = $twigConfig['templateDirs'];
